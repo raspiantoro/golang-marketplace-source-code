8afb9432df921425b25ab214d39f139ab9e70324
Mario Raspiantoro <raspiantoro@gmail.com>
2024-08-06 20:44:26 +0700

    add chapter 2.1

--- product-svc/main.go
+++ product-svc/main.go
@@ -1,7 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+)
+
+type ProductCategory struct {
+	ID   uint   `json:"id"`
+	Name string `json:"name"`
+}
 
 func main() {
-	fmt.Println("Hello, Product Services!")
+	r := chi.NewRouter()
+	r.Get("/api/product-category", func(w http.ResponseWriter, r *http.Request) {
+		categories := []ProductCategory{
+			{
+				ID:   1,
+				Name: "Alat Tulis",
+			},
+			{
+				ID:   2,
+				Name: "Perlengkapan Rumah Tangga",
+			},
+			{
+				ID:   3,
+				Name: "Fashion",
+			},
+		}
+
+		result, err := json.Marshal(&categories)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("Terjadi kesalahan pada server"))
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write(result)
+	})
+
+	fmt.Println("your server is running on port 8080")
+	http.ListenAndServe(":8080", r)
 }
